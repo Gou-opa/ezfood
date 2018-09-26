5645b51a81a5c2bae88c18f37edd7d88600e3bd4
Gou-Opa <monneymrt1998@gmail.com>
2018-09-27 03:26:26 +0700

    test all crud of order success

--- business_flow/BF_gateway.js
+++ business_flow/BF_gateway.js
@@ -1,7 +1,7 @@
 var mongo_handler = require('./../database/mongo_handler');
 var BFO = mongo_handler.BF.order;
 var objorder = {
-    dishname: "Lợn quay",
+    dishname: "Lợn hấp lá chanh",
     quantity: 2,
     option: 
     {
@@ -10,6 +10,38 @@ var objorder = {
         decoration: ""
     }
 };
-var BFAPI = BFO.order.add(objorder);
+var objlist = 
+[
+    {
+        dishname: "Lợn nướng",
+        quantity: 2,
+        option: 
+        {
+            ingredient: "Không hành",
+            cook: "Chín kỹ",
+            decoration: ""
+        }
+    },
+    {
+        dishname: "Lợn giả cầy",
+        quantity: 2,
+        option: 
+        {
+            ingredient: "",
+            cook: "Chín kỹ",
+            decoration: ""
+        }
+    }
+]
 
+//BFO.count({quantity: 2});
+//console.log(c2);
+//BFO.insertMany(objlist);
+//BFO.insertOne(objorder);
+//BFO.updateOne({dishname: "Lợn luộc"},{dishname:"Thịt chó"});
+//BFO.updateMany({dishname: "Lợn giả cầy"},{dishname:"Cháo trắng"});
+
+//BFO.find({dishname:"Cháo gà"});
+BFO.deleteOne({dishname: "Bò hầm"});
+BFO.deleteMany({dishname:"Lợn hấp lá chanh"});
 // file nay se duoc dung de gọi api xu ly order
--- business_flow/order/CRUD.js
+++ business_flow/order/CRUD.js
@@ -8,42 +8,72 @@ var pool = require('../../database/orderpool_schema');
 
 
 var order_api = {
-    add: function(objorder){
-        pool.create(objorder, function(err){
+    count: function(query_filter){
+        pool.count(query_filter, function(err,res){
             if(err) throw err;
-            else console.log("1 order required");
-        });
-        // mongodb.connect(url,function(err,db) {
-        //     if (err) throw err;
-        //     var dbo = db.db("ezfood");
-        //     //dbo.createCollection("orderpool");
-        //     dbo.collection("orderpool").insertOne(order, function(err, res) {
-        //       if (err) throw err;
-        //       console.log("1 order required");
-        //       db.close();
-        //     });
-        // });
-    },
-    modify : function(find_object_query, modified_order){
-        mongodb.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("ezfood");
-            dbo.collection("orderpool").updateOne(find_object_query, modified_order, function(err, res) {
-                if (err) throw err;
-                console.log("1 order updated");
-                db.close();
-            });
+            else console.log("counted "+ res);
         });
     },
-    delete: function(find_object_query){
-        mongodb.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("ezfood");
-            dbo.collection("orderpool").deleteOne(find_object_query, function(err, res) {
-                if (err) throw err;
-                console.log("1 order deleted");
-                db.close();
-            });
+    insertMany: function(objorderlist){
+        pool.insertMany(objorderlist, function(err, res){
+            if(err) throw err;
+            else {
+                console.log(objorderlist.length + " order added");
+                console.log(res);
+            }
+        });
+    },
+    insertOne: function(objorder){
+        pool.create(objorder, function(err,res){
+            if(err) throw err;
+            else {
+                console.log("1 order added");
+                console.log(res);
+            }
+        });
+    },
+    updateOne : function(find_object_query, modified_order){
+        pool.updateOne(find_object_query, {$set: modified_order}, function(err,res){
+            if(err) throw err;
+            else {
+                console.log("Updated "+ JSON.stringify(find_object_query) + " with " + JSON.stringify(modified_order));
+                console.log(res);
+            }
+        });
+    },
+    updateMany : function(find_object_query, modified_order){
+        pool.updateMany(find_object_query, {$set: modified_order}, function(err,res){
+            if(err) throw err;
+            else {
+                console.log("Updated many order contain "+ JSON.stringify(find_object_query) + " with " + JSON.stringify(modified_order));
+                console.log(res);
+            }
+        });
+    },
+    find: function(find_object_query){
+        pool.find(find_object_query, function(err,res){
+            if(err) throw err;
+            else console.log(JSON.stringify(res));
+        });
+        
+    },
+    deleteOne: function(find_object_query){
+        pool.deleteOne(find_object_query, function(err,res){
+            if(err) throw err;
+            else {
+                console.log("Deleted "+ find_object_query);
+                console.log(res);
+            }
+        });
+    },
+    
+    deleteMany: function(find_object_query){
+        pool.deleteMany(find_object_query, function(err,res){
+            if(err) throw err;
+            else {
+                console.log("Deleted "+ find_object_query);
+                console.log(res);
+            }
         });
     }
 };
