22bdbb894a458dc3ea19962556d3cbca3a67da48
vnlong198 <lvip300@gmail.com>
2018-11-28 10:09:47 +0700

    delete back

--- ezfood_backend/routes/images.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/:id', function(req,res){
-    res.sendFile("/home/gou/Dev/ezfood/images/"+req.params.id);
-})
-module.exports = router;
