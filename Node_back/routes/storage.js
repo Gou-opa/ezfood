f5760885ce99299da4210220fc07d414fbd4f3ab
sweelt0ve98 <loveparadise98@gmail.com>
2018-12-15 01:41:55 +0700

    add api add and delete ingredient

--- Node_back/routes/storage.js
+++ Node_back/routes/storage.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 //var foodtype = require('./../database/foodtype_schema.js');
-var ingredientpool = require('./../database/ingredient_schema.js');
+var ingredientpool = require('./../database/ingredient_schema');
 const authorize  = require('./../business_flow/user/checkAuthorized');
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -95,4 +95,48 @@ router.get('/', function(req, ress){
   });
 });
 */
+router.post("/addingredient/:uid", function(req,res){
+  authorize.checker(req.params.uid, function(role){
+    console.log(role);
+    if( role == authorize.ADMIN){
+      var ingredientForm = req.body;
+      console.log("adding ingredient " + JSON.stringify(ingredientForm));
+      ingredientpool.create(ingredientForm, function(err,result){
+        if(err) {
+          console.log("insert ingredient failed");
+          res,status(500).json({});
+        }
+        else {
+          console.log("1 ingredient added to storage");
+          console.log(result);
+          res.status(200).json({});
+        }
+      });
+    }
+    else if(authorize.USER  == role) res.status(403).json(authorize.unauthorized_message);
+    else res.status(402).json(authorize.guess);
+  });
+});
+
+router.delete('/delingredient/:uid', function(req, res){
+  authorize.checker(req.params.uid, function(role){
+    console.log(role);
+    if( role == authorize.ADMIN){
+      var iId = req.body._id;
+      ingredientpool.findOneAndDelete({"_id": iId }, function(err, ressu){
+        if(err) {
+          console.log("xoa failed");
+          res.status(403).json({});
+        }
+        else {
+          console.log("da xoa ingredient "+ iId);
+          res.status(200).json({});
+        }
+      })
+    }
+    else if(authorize.USER  == role) res.status(403).json(authorize.unauthorized_message);
+    else res.status(402).json(authorize.guess);
+  });
+});
+
 module.exports = router;
