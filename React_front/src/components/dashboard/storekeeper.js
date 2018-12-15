54944b30ffc7a69a20a4e75bc006d8945c99f7b7
Nguyen Van Long <lvip300@gmail.com>
2018-12-15 09:38:45 +0700

    Update storekeeper.js

--- React_front/src/components/dashboard/storekeeper.js
+++ React_front/src/components/dashboard/storekeeper.js
@@ -7,11 +7,13 @@ import {Card, CardHeader, CardBody, CardTitle } from 'reactstrap';
 // react table
 import ReactTable from "react-table";
 import callApi from '../../service/APIservice'
+//authentication
+import {uid} from "../../service/auth"
 
 class History extends Component{
 
     componentWillMount() {
-        callApi( 'storekeeper/all', 'GET', null).then(res => {
+        callApi( `storage/all/${uid}`, 'GET', null).then(res => {
             // console.log(res.data.storage);
            this.setState({
                data : res.data.storage
@@ -28,94 +30,8 @@ class History extends Component{
 
 
     render(){
-        const  data  =
-            [
-                {
-                    "name" : "Thịt Mông Sấn Lợn",
-                    "quantity": "9kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "15-12-2018",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Bột mì",
-                    "quantity": "5kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "12-12-2019",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Tôm Sú Nguyên Con",
-                    "quantity": "12 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "29-12-2018",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Bắp Bò Úc",
-                    "quantity": "24 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "12-05-2019",
-                    "status": "NCC đang vận chuyển"
-                },
-                {
-                    "name" : "Gầu Bò Úc ",
-                    "quantity": "14 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "12-05-2019",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Ba Chỉ Bò Mỹ ",
-                    "quantity": "25 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "12-05-2019",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Cá Hồi Tươi Nauy",
-                    "quantity": "5 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "25-12-2018",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Mực Trứng Cô Tô ",
-                    "quantity": "10 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "29-12-2018",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Lươn Biển Nhật Bản Unagi",
-                    "quantity": "6 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "29-12-2018",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Thịt Nạc Xay",
-                    "quantity": "3 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "14-12-2018",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Móng Giò Lợn",
-                    "quantity": "6 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "14-12-2018",
-                    "status": "Đã lưu kho"
-                },
-                {
-                    "name" : "Thịt bê",
-                    "quantity": "6 kg",
-                    "import-date": "12-12-2018",
-                    "expire-date": "14-12-2018",
-                    "status": "Đã lưu kho"
-                }
-                
-            ]
+        const  {data}  = this.state;
+            
         return(
                 <Card style={{margin: "10px 0px 0px 0px"}}>
                     <CardHeader >
@@ -145,18 +61,13 @@ class History extends Component{
                             },
                             {
                                 width: "22%",
-                                Header: "Ngày nhập",
-                                accessor: "import-date"
+                                Header: "Ngày nhận",
+                                accessor: "received_date"
                             },
                             {
                                 width: "22%",
                                 Header: "Hạn dùng",
-                                accessor: "expire-date"
-                            },
-                            {
-                                width: "10%",
-                                Header: "Trạng thái",
-                                accessor: "status",
+                                accessor: "expire"
                             }
                             ]
                         }
